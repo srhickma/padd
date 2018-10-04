b1ce75e3872ef487a55d7746ba94d278f459b255
Shane Hickman <srhickma@edu.uwaterloo.ca>
2018-10-04 00:07:11 -0400

    Stream visibility and oop improvements

--- src/core/data/stream.rs
+++ src/core/data/stream.rs
@@ -6,8 +6,6 @@ pub struct StreamSource<'g, T: 'g + Clone> {
 }
 
 impl<'g, T: 'g + Clone> StreamSource<'g, T> {
-    //TODO reduce visibility
-
     pub fn observe(getter: &'g mut FnMut() -> Option<T>) -> StreamSource<'g, T> {
         StreamSource {
             consumers: LinkedList::new(),
@@ -16,24 +14,21 @@ impl<'g, T: 'g + Clone> StreamSource<'g, T> {
     }
 
     pub fn split<'a>(&'a mut self) -> Stream<'a, 'g, T> {
-        self.consumers.push_front(StreamBuffer {
-            incoming_buffer: LinkedList::new(),
-            outgoing_buffer: LinkedList::new()
-        });
+        self.consumers.push_front(StreamBuffer::new());
 
         self.back().unwrap()
     }
 
-    pub fn detach_front<'a>(&'a mut self) {
+    fn detach_front<'a>(&'a mut self) {
         self.consumers.pop_front();
     }
 
-    pub fn detach_back<'a>(&'a mut self) -> Option<Stream<'a, 'g, T>> {
+    fn detach_back<'a>(&'a mut self) -> Option<Stream<'a, 'g, T>> {
         self.consumers.pop_front();
         self.back()
     }
 
-    pub fn back<'a>(&'a mut self) -> Option<Stream<'a, 'g, T>> {
+    fn back<'a>(&'a mut self) -> Option<Stream<'a, 'g, T>> {
         Some(Stream {
             source: self,
             buffer: match self.consumers.back_mut() {
@@ -43,7 +38,7 @@ impl<'g, T: 'g + Clone> StreamSource<'g, T> {
         })
     }
 
-    pub fn pull(&mut self) {
+    fn pull(&mut self) {
         match (self.getter)() {
             None => {},
             Some(val) => for consumer in &mut self.consumers {
@@ -59,8 +54,42 @@ pub struct StreamBuffer<T: Clone> {
 }
 
 impl<T: Clone> StreamBuffer<T> {
+    fn new() -> Self {
+        StreamBuffer {
+            incoming_buffer: LinkedList::new(),
+            outgoing_buffer: LinkedList::new()
+        }
+    }
+
     fn push(&mut self, val: &T) {
         self.incoming_buffer.push_front(val.clone())
+    }
+
+    fn dequeue_incoming(&mut self) -> Option<T> {
+        self.incoming_buffer.pop_back()
+    }
+
+    fn enqueue_outgoing(&mut self, val: &T) {
+        self.outgoing_buffer.push_back(val.clone());
+    }
+
+    fn input_queued(&self) -> bool {
+        self.incoming_buffer.is_empty()
+    }
+
+    fn advance(&mut self) {
+        match self.incoming_buffer.pop_back() {
+            None => {},
+            Some(val) => self.outgoing_buffer.push_back(val.clone())
+        };
+    }
+
+    fn replay(&mut self) {
+        let mut val: Option<T> = self.outgoing_buffer.pop_back();
+        while val.is_some() {
+            self.incoming_buffer.push_back(val.unwrap());
+            val = self.outgoing_buffer.pop_back();
+        }
     }
 }
 
@@ -89,27 +118,23 @@ impl<'a, 'g: 'a, T: 'g + 'a + Clone> Stream<'a, 'g, T> {
     }
 
     pub fn consumer<'s>(&'s mut self, on_consume: Box<FnMut(&LinkedList<T>) + 's>) -> StreamConsumer<'s, 'a, 'g, T> {
-        StreamConsumer {
-            stream: self,
-            on_consume,
-            block: false
-        }
+        StreamConsumer::new(self, on_consume)
     }
 
     pub fn has_next(&mut self) -> bool {
-        if self.buffer.incoming_buffer.is_empty() {
+        if self.buffer.input_queued() {
             self.source_pull();
-            !self.buffer.incoming_buffer.is_empty()
+            !self.buffer.input_queued()
         } else {
             true
         }
     }
 
     pub fn pull(&mut self) -> Option<T> {
-        let val: T = match self.buffer.incoming_buffer.pop_back() {
+        let val: T = match self.buffer.dequeue_incoming() {
             None => {
                 self.source_pull();
-                match self.buffer.incoming_buffer.pop_back() {
+                match self.buffer.dequeue_incoming() {
                     None => return None,
                     Some(val) => val
                 }
@@ -117,34 +142,25 @@ impl<'a, 'g: 'a, T: 'g + 'a + Clone> Stream<'a, 'g, T> {
             Some(val) => val
         };
 
-        self.buffer.outgoing_buffer.push_back(val.clone());
+        self.buffer.enqueue_outgoing(&val);
 
         Some(val)
+    }
+
+    pub fn advance(&mut self) -> &mut Self {
+        self.buffer.advance();
+        self
+    }
+
+    pub fn replay(&mut self) -> &mut Self {
+        self.buffer.replay();
+        self
     }
 
     fn source_pull(&mut self) {
         unsafe {
             (&mut *self.source).pull()
         }
-    }
-
-    pub fn advance(&mut self) -> &mut Self {
-        match self.buffer.incoming_buffer.pop_back() {
-            None => self,
-            Some(val) => {
-                self.buffer.outgoing_buffer.push_back(val.clone());
-                self
-            }
-        }
-    }
-
-    pub fn replay(&mut self) -> &mut Self {
-        let mut val: Option<T> = self.buffer.outgoing_buffer.pop_back();
-        while val.is_some() {
-            self.buffer.incoming_buffer.push_back(val.unwrap());
-            val = self.buffer.outgoing_buffer.pop_back();
-        }
-        self
     }
 }
 
@@ -155,6 +171,14 @@ pub struct StreamConsumer<'s, 'a: 's, 'g: 'a + 's, T: 'g + 'a + 's + Clone> {
 }
 
 impl<'s, 'a: 's, 'g: 'a + 's, T: 'g + 'a + 's + Clone> StreamConsumer<'s, 'a, 'g, T> {
+    fn new(stream: &'s mut Stream<'a, 'g, T>, on_consume: Box<FnMut(&LinkedList<T>) + 's>) -> Self {
+        StreamConsumer {
+            stream,
+            on_consume,
+            block: false
+        }
+    }
+
     pub fn has_next(&mut self) -> bool {
         self.stream.has_next()
     }
