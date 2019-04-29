a41c0d1e66ac5b638dcfbf1404539a57b7a4e60c
Shane Hickman <srhickma@edu.uwaterloo.ca>
2019-04-29 16:33:09 -0400

    Add cli test for file regexes

--- src/main.rs
+++ src/main.rs
@@ -223,7 +223,43 @@ mod tests {
 
     #[test]
     fn test_file_regex() {
-        //TODO(shane)
+        //setup
+        let _ = fs::remove_dir_all(&*TEMP_DIR);
+        fs::create_dir(&*TEMP_DIR).unwrap();
+
+        let mut testable_files: Vec<TestableFile> = Vec::new();
+
+        for file_name in files_with_prefix("") {
+            let file = TestableFile::new(file_name);
+            file.copy_to_temp();
+            testable_files.push(file);
+        }
+
+        //exercise
+        cli::run(vec![
+            "padd",
+            "fmt",
+            "tests/spec/lacs",
+            "-t",
+            &TEMP_DIR.to_string_lossy().to_string(),
+            "-m",
+            "lacs_.*",
+        ]);
+
+        //verify
+        let mut formatted: usize = 0;
+        assert!(testable_files.len() > 3);
+        for file in testable_files {
+            if file.file_name.starts_with("lacs_") {
+                file.assert_matches_output();
+                formatted += 1;
+            }
+        }
+
+        assert_eq!(formatted, 3);
+
+        //teardown
+        fs::remove_dir_all(&*TEMP_DIR).unwrap();
     }
 
     #[test]
