f2260bf254ffbf2410b10dc78ef3ebb8774b24fb
Shane Hickman <srhickma@edu.uwaterloo.ca>
2019-04-30 02:15:12 -0400

    Add documentation for CEHashMap (#147)

--- src/core/data/map.rs
+++ src/core/data/map.rs
@@ -1,13 +1,30 @@
+/// Continuously Encoded Hash Map: Keys are integers which index values directly in a vector.
+/// Efficient when the key space is very dense (e.g. when storing half a bijection).
+///
+/// # Type Parameters
+///
+/// * `V` - the type of the values stored in the map.
+///
+/// # Fields
+///
+/// * `vector` - the underlying vector which stores the values of the map, and is indexed by CEHashMap keys.
 pub struct CEHashMap<V: Default> {
     vector: Vec<Option<V>>,
 }
 
 #[allow(dead_code)]
 impl<V: Default> CEHashMap<V> {
+    /// Returns a new empty CEHashMap.
     pub fn new() -> CEHashMap<V> {
         CEHashMap { vector: Vec::new() }
     }
 
+    /// Inserts a value into the map at the given key.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - the key at which to insert the value in the map.
+    /// * `value` - the value to be inserted into the map.
     pub fn insert(&mut self, key: usize, value: V) {
         while self.vector.len() <= key {
             self.vector.push(None);
@@ -15,6 +32,11 @@ impl<V: Default> CEHashMap<V> {
         self.vector[key] = Some(value);
     }
 
+    /// Returns a reference to the value stored in the map at the given key, or `None` if no such value is stored.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - the key to look up in the map.
     pub fn get(&self, key: usize) -> Option<&V> {
         match self.vector.get(key) {
             None => None,
@@ -22,6 +44,11 @@ impl<V: Default> CEHashMap<V> {
         }
     }
 
+    /// Returns a _mutable_ reference to the value stored in the map at the given key, or `None` if no such value is stored.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - the key to look up in the map.
     pub fn get_mut(&mut self, key: usize) -> Option<&mut V> {
         match self.vector.get_mut(key) {
             None => None,
@@ -29,14 +56,21 @@ impl<V: Default> CEHashMap<V> {
         }
     }
 
+    /// Returns `true` if there is a value stored in the map under the given key, and `false` otherwise.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - the key to look up in the map.
     pub fn contains(&self, key: usize) -> bool {
         self.get(key).is_some()
     }
 
+    /// Returns the size of the map (i.e. the number of values stored).
     pub fn size(&self) -> usize {
         self.vector.len()
     }
 
+    /// Returns an iterator over the values of the map, which starts at low keys and iterates to high keys.
     pub fn iter(&self) -> CEHashMapIterator<V> {
         CEHashMapIterator {
             map: &self,
@@ -45,6 +79,16 @@ impl<V: Default> CEHashMap<V> {
     }
 }
 
+/// Continuously Encoded Hash Map Iterator: An iterator over the values of a CEHashMap.
+///
+/// # Type Parameters
+///
+/// * `V` - the type of the values stored in the map.
+///
+/// # Fields
+///
+/// * `map` - a reference to the map being iterated over.
+/// * `index` - the index (key) of the current element in the map.
 pub struct CEHashMapIterator<'scope, V: Default + 'scope> {
     map: &'scope CEHashMap<V>,
     index: usize,
@@ -52,6 +96,9 @@ pub struct CEHashMapIterator<'scope, V: Default + 'scope> {
 
 impl<'scope, V: Default + 'scope> Iterator for CEHashMapIterator<'scope, V> {
     type Item = &'scope V;
+
+    /// Returns a reference to the current value in the iteration, or `None` if the iteration is complete.
+    /// The current value is then set to the next value in the iteration.
     fn next(&mut self) -> Option<&'scope V> {
         while self.index < self.map.size() {
             let entry = self.map.get(self.index);
