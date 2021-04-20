b4821b5b58f3eab7af9e8e6ffb6ff12deb5148ea
Shane Hickman <shanerhickman@gmail.com>
2021-04-20 18:56:11 -0400

    WIP configuration file changes

--- src/cli/configuration.rs
+++ src/cli/configuration.rs
@@ -2,21 +2,36 @@ extern crate serde;
 extern crate serde_yaml;
 
 use {
-    serde::Deserialize,
+    self::serde::Deserialize,
     std::{
-        error,
-        fmt,
+        collections::HashMap,
+        error, fmt,
         fs::{self, File, OpenOptions},
-        io::{Read, Seek, SeekFrom, Write},
+        io::{Read, Seek, SeekFrom},
         path::Path,
     },
 };
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Configuration {
-    pub spec: Option<String>,
+    pub flags: Option<Flags>,
+    pub groups: HashMap<String, MatcherGroup>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct MatcherGroup {
+    pub flags: Option<Flags>,
+    pub pipe: Vec<String>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Flags {
     pub target: Option<String>,
-    pub
+    pub threads: Option<usize>,
+    pub no_skip: Option<bool>,
+    pub no_track: Option<bool>,
+    pub no_write: Option<bool>,
+    pub check: Option<bool>,
 }
 
 pub fn read_configuration(path: &Path) -> Result<Configuration, ConfigurationError> {
@@ -27,14 +42,16 @@ pub fn read_configuration(path: &Path) -> Result<Configuration, ConfigurationErr
             if let Err(err) = file.read_to_string(&mut conf_str) {
                 return Err(ConfigurationError::IOErr(format!(
                     "Could not read configuration file \"{}\": {}",
-                    &path, err
+                    &path.to_string_lossy().to_string(),
+                    err
                 )));
             }
         }
         Err(err) => {
             return Err(ConfigurationError::IOErr(format!(
                 "Could not find configuration file \"{}\": {}",
-                &path, err
+                &path.to_string_lossy().to_string(),
+                err
             )));
         }
     }
@@ -51,7 +68,9 @@ impl fmt::Display for ConfigurationError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             ConfigurationError::IOErr(ref err) => write!(f, "IO Error: {}", err),
-            ConfigurationError::DeserializationErr(ref err) => write!(f, "Failed to parse configuration file: {}", err),
+            ConfigurationError::DeserializationErr(ref err) => {
+                write!(f, "Failed to parse configuration file: {}", err)
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ extern crate lazy_static;
 extern crate clap;
 #[macro_use]
 extern crate log;
+#[macro_use]
+extern crate serde;
 extern crate backtrace;
 extern crate padd;
 
